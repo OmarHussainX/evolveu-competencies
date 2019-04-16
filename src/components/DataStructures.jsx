3b16dec6824476d2f657aedf43a2240584a4362f
Omar Hussain <omar.hussain@gmail.com>
2019-04-15 22:02:05 -0600

    more progress on UI for DataStructures

--- src/components/DataStructures.jsx
+++ src/components/DataStructures.jsx
@@ -8,18 +8,25 @@ class DataStructures extends Component {
         super()
         this.state = {
             dataStructureChoice: '',
+            movieTitle: '',
+            movieGross: '',
         }
     }
     
     // Ensures state always contains the latest input for:
     // - data structure choice
     changeHandler = event => {
-        const { name, value } = event.target
-        console.log(`Selection ${name}: ${value}`)
+        const { name, value } = event.currentTarget
+        console.log(`--- changeHandler() ---\n${name}: ${value}`)
 
             this.setState({
                 [name]: value,
             })
+    }
+
+    clickHandler = event => {
+        const { id } = event.currentTarget
+        console.log(`--- '${id}' clicked ---`)
     }
 
     render() {
@@ -33,19 +40,18 @@ class DataStructures extends Component {
                 >
                     <option value='' disabled>Select a data structure</option>
                     <optgroup label="Pre-filled with test data">
-                        <option value="llist">Linked List</option>
+                        <option value="llist">Linked list</option>
                         <option value="queue">Queue (FIFO)</option>
                         <option value="stack">Stack (LIFO)</option>
                     </optgroup>
                     <optgroup label="Empty (no pre-filled data)">
-                        <option value="llist-empty">Linked List</option>
+                        <option value="llist-empty">Linked list</option>
                         <option value="queue-empty">Queue (FIFO)</option>
                         <option value="stack-empty">Stack (LIFO)</option>
                     </optgroup>
                 </select>   
                 <label>
                     <input
-                        className=''
                         type='text'
                         placeholder='Movie title'
                         name='movieTitle'
@@ -55,7 +61,6 @@ class DataStructures extends Component {
                 </label>
                 <label>
                     <input
-                        className=''
                         type="number"
                         placeholder='&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gross (in M)'
                         min="0"
@@ -70,20 +75,20 @@ class DataStructures extends Component {
                     Insert
                 </button>
                 <div id='positionControlsWrapper'>
-                    <button id='pos_first' onClick={this.props.clickHandler}>
+                    <button id='pos_first' onClick={this.clickHandler}>
                         <FontAwesomeIcon icon='angle-double-left' size="2x"></FontAwesomeIcon>
                     </button>
-                    <button id='pos_prev' onClick={this.props.clickHandler}>
+                    <button id='pos_prev' onClick={this.clickHandler}>
                         <FontAwesomeIcon icon='angle-left' size="2x"></FontAwesomeIcon>
                     </button>
-                    <button id='pos_next' onClick={this.props.clickHandler}>
+                    <button id='pos_next' onClick={this.clickHandler}>
                         <FontAwesomeIcon icon='angle-right' size="2x"></FontAwesomeIcon>
                     </button>
-                    <button id='pos_last' onClick={this.props.clickHandler}>
+                    <button id='pos_last' onClick={this.clickHandler}>
                         <FontAwesomeIcon icon='angle-double-right' size="2x"></FontAwesomeIcon>
                     </button>
                 </div>
-                <button id='delete' onClick={this.clickHandler}>
+                <button id='delete' onClick={this.clickHandler} className='severe'>
                     <FontAwesomeIcon icon='minus-circle' size="sm" style={{marginRight:'5px'}}></FontAwesomeIcon>
                     Delete
                 </button>
