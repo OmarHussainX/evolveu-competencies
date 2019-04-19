ac15cfbfd4bd39bb6d87fe96c64bcbfcf7ecd74c
Omar Hussain <omar.hussain@gmail.com>
2019-04-19 16:21:17 -0600

    begin TDD for City, wrote tests and stubs

---
+++ src/components/js/City.js
@@ -0,0 +1,34 @@
+class City {
+    constructor(name = null, latitude = 0, longtitude = 0, population = 0) {
+        this.name = name
+        this.latitude = latitude
+        this.longtitude = longtitude
+        this.population = population
+    }
+
+
+    // Creates a String representation of the City
+    show() {
+        //return `this.name [this.latitude, this.longtiude] (pop. ${this.population})`
+    }
+
+
+    //  Receives a number that will be added to the city’s population
+    movedIn() {
+
+    }
+
+
+    //  Receives a number that will be subtracted from the city’s population
+    movedOut() {
+
+    }
+
+
+    //  Returns a String representing the scale of the city based on its population
+    howBig() {
+
+    }
+}
+
+export { City }
