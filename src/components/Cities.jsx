ea987a1beecbd585611ef0941790eb056c92aa55
Omar Hussain <omar.hussain@gmail.com>
2019-04-20 21:42:45 -0600

    completed first pass of adding rendering logic

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './css/Cities.css'
 
-// Data set to use if a pre-filled data structure is selected
-import movieData from './js/movies.json'
+// Data set to use
+import citiesData from './js/cities.json'
+import { Community } from "./js/Community";
 
 // Set to 'true' to enable output of debug messages in console
-const DEBUG_MSG = false
+const DEBUG_MSG = true
 
 
 
@@ -20,32 +21,63 @@ class Cities extends Component {
     constructor() {
         super()
         this.state = {
-            dataSet: null,
+            community: new Community(citiesData),
             hemisphereChoice: 'both',
         }
     }
 
     
     // When a selection is made, updates state with:
-    // - the data structure selected
-    // - reference to the data set to be displayed (if any)
+    // - choice of hemisphere to show: both, Northern, or Southern
+    // - reference to the Community to be displayed
     changeHandler = event => {
         const { name, value } = event.currentTarget
         if (DEBUG_MSG) console.log(`----- Cities changeHandler()\n${name}: ${value}`)
 
-        // If an 'empty' data structure has been selected, save reference
-        // to an empty array in state, otherwise save reference to test data
-        const selectedData = value.includes('-empty') ? [] : movieData
+        // Create a new Community containgin all cities, and
+        // filter out Southern or Northern cities if needed
+        let newCommunity = new Community(citiesData)
+        if  (value === 'north' || value === 'south')
+           newCommunity = newCommunity.whichSphere(value) 
 
         this.setState({
-            dataSet: selectedData,
+            community: newCommunity,
             [name]: value
         })
     }
 
     
     render() {
-        const { dataSet, hemisphereChoice } = this.state
+        const { community, hemisphereChoice } = this.state
+
+        const citiesList = community.cities.map((city, i ) => {
+
+            let population = city.population
+            if (population >= 1000000)
+                population = `${(population/1000000.0).toFixed(1)}M`
+            else if (population >= 1000)
+                population = `${(population/1000.0).toFixed(1)}k`
+            
+            return (
+                <tr key={i}>
+                    <td>
+                        {city.name}<br/>
+                        <span>{city.howBig()}</span>
+                    </td>
+                    <td>{population}</td>
+                    <td>
+                        <button>
+                            <FontAwesomeIcon icon='plus-circle' size="sm"></FontAwesomeIcon>
+                        </button>
+                        &nbsp;
+                        <button>
+                            <FontAwesomeIcon icon='minus-circle' size="sm"></FontAwesomeIcon>
+                        </button>
+                    </td>
+                    <td>{`${city.latitude.toFixed(2)},${city.latitude.toFixed(2)}`}</td>
+                </tr>
+            )
+        })
 
         return (
             <div id='Cities'>
@@ -56,28 +88,29 @@ class Cities extends Component {
                         value={hemisphereChoice}
                         onChange={this.changeHandler}
                         name="hemisphereChoice">
-                        <option value="all">Show cities in both hemispheres</option>
-                        <option value="northern">Northern hemisphere only</option>
-                        <option value="southern">Southern hemisphere only</option>
+                        <option value="both">Show cities in both hemispheres</option>
+                        <option value="north">Northern hemisphere only</option>
+                        <option value="south">Southern hemisphere only</option>
                     </select>
                 </div>
                 <table className='overview'>
                     <tbody>
                         <tr>
                             <td>Northern-most</td>
-                            <td>Some city.....</td>
+                            <td>{community.getMostNorthern().name}</td>
                         </tr>
                         <tr>
                             <td>Southern-most</td>
-                            <td>Some other city....</td>
+                            <td>{community.getMostSouthern().name}</td>
                         </tr>
                         <tr>
                             <td>Total pop.</td>
-                            <td>{(134567245).toLocaleString(undefined)}</td>
+                            <td>{community.getPopulation().toLocaleString(undefined)}</td>
                         </tr>
                     </tbody>
                 </table>
                 <div className='citieslistCard'>
+
                 <table className='citieslist'>
                     <tbody>
                         <tr>
@@ -85,42 +118,10 @@ class Cities extends Component {
                             <th colSpan='2'>Population</th>
                             <th>latitude, longitude</th>
                         </tr>
-                        <tr>
-                            <td>
-                                Calgary<br/>
-                                <span>(City)</span>
-                            </td>
-                            <td>1.3M</td>
-                            <td>
-                                <button>
-                                    <FontAwesomeIcon icon='plus-circle' size="sm"></FontAwesomeIcon>
-                                </button>
-                                &nbsp;
-                                <button>
-                                    <FontAwesomeIcon icon='minus-circle' size="sm"></FontAwesomeIcon>
-                                </button>
-                            </td>
-                            <td>45.34, -52.45</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Yellowknife<br/>
-                                <span>(Large town)</span>
-                            </td>
-                            <td>500k</td>
-                            <td>
-                                <button>
-                                    <FontAwesomeIcon icon='plus-circle' size="sm"></FontAwesomeIcon>
-                                </button>
-                                &nbsp;
-                                <button>
-                                    <FontAwesomeIcon icon='minus-circle' size="sm"></FontAwesomeIcon>
-                                </button>
-                            </td>
-                            <td>72.81, -122.36</td>
-                        </tr>
+                        {citiesList}
                     </tbody>
                 </table>
+
                 </div>
             </div>
         )
